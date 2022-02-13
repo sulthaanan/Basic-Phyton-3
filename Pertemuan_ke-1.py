1860c2c024820016aeddebeb0191cbe9db99ba60
sulthaanan <99521952+sulthaanan@users.noreply.github.com>
2022-02-13 13:38:20 +0700

    Add files via upload

---
+++ Pertemuan_ke-1.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # First Program
+
+# ## Second Program
+
+# ### Third Program
+
+# #### Fourth Program
+
+# In[1]:
+
+
+# contoh comment
+
+
+# In[2]:
+
+
+print('hello word')
+
+
+# In[3]:
+
+
+print('Kelas basic pyhton batch 13')
+
+
+# Contoh markdown
+
+# In[4]:
+
+
+salam = 'Selamat siang'
+print(salam)
+
+
+# In[5]:
+
+
+print('Budi', 123, 'Dadang')
+
+
+# In[7]:
+
+
+integer = 100
+print(integer)
+
+
+# In[8]:
+
+
+salam
+
+
+# In[9]:
+
+
+desimal = 3.6
+print (desimal)
+
+
+# In[10]:
+
+
+comp = 1 + 3j
+print(comp)
+
+
+# In[11]:
+
+
+# melihat tipe data
+
+
+# In[12]:
+
+
+type (comp)
+
+
+# In[13]:
+
+
+type(salam)
+
+
+# In[14]:
+
+
+type(integer)
+
+
+# In[15]:
+
+
+type(desimal)
+
+
+# In[16]:
+
+
+string = 'Basic Python batch 13'
+print (string)
+print(type(string))
+
+
+# In[17]:
+
+
+# operasi matematika
+
+
+# In[18]:
+
+
+50-20
+
+
+# In[19]:
+
+
+a = 10
+b = 2
+kali = a*b
+print(kali)
+
+
+# In[20]:
+
+
+kali
+
+
+# In[21]:
+
+
+10*2
+
+
+# In[22]:
+
+
+a = 10
+b = 2
+bagi = a/b
+print(bagi)
+print(type(bagi))
+
+
+# In[23]:
+
+
+a = 10
+b = 3
+bagi2 = a/b
+print(bagi2)
+print(type(bagi2))
+
+
+# In[24]:
+
+
+a = 10
+b = 2
+bagi3 = a//b
+print(bagi3)
+print(type(bagi3))
+
+
+# In[25]:
+
+
+a = 10
+b = 3
+bagi4 = a//b
+print(bagi4)
+print(type(bagi4))
+
+
+# In[26]:
+
+
+c = 17
+d = 3
+md = c%d
+print(md)
+
+
+# In[27]:
+
+
+e = 15
+f = 3
+ms = e%f
+print(ms)
+
+
+# In[28]:
+
+
+# casting
+
+
+# In[29]:
+
+
+x = str(desimal)
+print(x)
+print(type(x))
+
+
+# In[30]:
+
+
+y = float(integer)
+print(y)
+print(type(y))
+
+
+# In[31]:
+
+
+s = int('5')
+print(s)
+print(type(s))
+
+
+# In[32]:
+
+
+k = int('5.2')
+print(k)
+print(type(k))
+
+
+# In[33]:
+
+
+p = int('eksperimen')
+print(p)
+print(type(p))
+
+
+# Kesimpulan: Semua data integer bisa dikonversi ke string namun tidak semua data string bisa dikonversi ke integer
+
+# In[34]:
+
+
+j = int('5.2')
+print(j)
+print(type(j)
+
+
+# In[35]:
+
+
+t = str(1750)
+print(t)
+print(type(t))
+
+
+# In[ ]:
+
+
+help()
+
+
+# In[ ]:
+
+
+
+
